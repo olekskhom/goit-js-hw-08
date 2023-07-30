9127632ce97561c62052cfc30b0f72ee1743e7aa
olekskhom <oleks.khom@gmail.com>
2023-07-30 15:59:32 +0300

    task-1_t

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,5 @@
-
-
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 
 import { galleryItems } from './gallery-items';
@@ -7,3 +7,25 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+function createGalleryCardMurkup(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+    `;
+    })
+    .join('');
+}
+
+const cardsMarcup = createGalleryCardMurkup(galleryItems);
+galleryEl.innerHTML = cardsMarcup;
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: `alt`,
+  captionDelay: 250,
+});
