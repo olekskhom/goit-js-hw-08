20c156fad75201c77869ea473b82567375779677
olekskhom <oleks.khom@gmail.com>
2023-07-30 16:47:38 +0300

    task-2_1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,13 +3,25 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 
- const iframe = document.querySelector('iframe');
-    const player = new Vimeo.Player(iframe);
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
+    // player.on('play', function() {
+    //     console.log('played the video!');
+    // });
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
+    // player.getVideoTitle().then(function(title) {
+    //     console.log('title:', title);
+    // });
+
+const STORAGE_PLAYER_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onPlayerTimeupdate, 1000));
+
+function onPlayerTimeupdate(data) {
+  const currenTime = data.seconds;
+
+  localStorage.setItem(STORAGE_PLAYER_KEY, currenTime);
+}
+
+player.setCurrentTime(localStorage.getItem(STORAGE_PLAYER_KEY) || 0);
